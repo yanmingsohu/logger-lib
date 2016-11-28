1abf56452bae0afc5f319a1374b945afb90da0cc
yanming <yanming-sohu@sohu.com>
2016-11-28 18:41:24 +0800

    自动创建目录, 判断参数
    Signed-off-by: yanming <yanming-sohu@sohu.com>

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 var config = {
-  
+
   logger : {
     // ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, MARK, OFF
     logLevel : 'ALL',
@@ -7,11 +7,14 @@ var config = {
     // 相对于项目目录的日志文件目录, 也可使用绝对目录
     log_dir  : 'logs',
 
+    // 如果目录不存在, 则创建(迭代)
+    create_dir : true,
+
     // 达到 log_size 后文件分块
     log_size : 20 * 1024 * 1024, // 20MB
 
     // 文件分块后, 最多保留几个分块文件
-    reserve_count : 10, 
+    reserve_count : 10,
 
     // 写 kfk 日志时需要的参数
     kfk_log: {
@@ -33,11 +36,11 @@ var config = {
     connect_delay     : 10 * 1000,
     conn_timeout      : 30 * 1000,
 
-    consumer_option   : { 
-      autoCommit      : true, 
-      fromBeginning   : false, 
-      fetchMaxWaitMs  : 1000, 
-      fetchMaxBytes   : 1024*1024 
+    consumer_option   : {
+      autoCommit      : true,
+      fromBeginning   : false,
+      fetchMaxWaitMs  : 1000,
+      fetchMaxBytes   : 1024*1024
     }
   }
 };
